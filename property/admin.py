29d8d4fd0b748e547b115939bbb19aa59df8d3f7
Igor Derevnin <shroom2207@yandex.ru>
2024-12-04 22:36:06 +0300

    Добавил декораторы

--- property/admin.py
+++ property/admin.py
@@ -6,27 +6,24 @@ from .models import Flat, Feedback, Owner
 class OwnerInstanceInline(admin.StackedInline):
     model = Owner.flats.through
     raw_id_fields = ('owner',)
-    
-    
+
+@admin.register(Flat)
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ('town', 'address')
     readonly_fields = ['created_at']
-    list_display = ('address', 'price', 'new_building', 'construction_year', 'town')
+    list_display = ('address', 'price', 'new_building',
+                    'construction_year', 'town')
     list_editable = ('new_building',)
     list_filter = ('new_building',)
     raw_id_fields = ('liked_by',)
     inlines = (OwnerInstanceInline,)
-    
-    
+
+@admin.register(Feedback)
 class FeedbackAdmin(admin.ModelAdmin):
-    raw_id_fields = ('flat_id',)
+    raw_id_fields = ('flat',)
     search_fields = ('author',)
-    
+
+@admin.register(Owner)
 class OwnerAdmin(admin.ModelAdmin):
     raw_id_fields = ('flats',)
-    search_fields = ('owner',) 
-
-admin.site.register(Flat, FlatAdmin)
-admin.site.register(Feedback, FeedbackAdmin) 
-admin.site.register(Owner, OwnerAdmin) 
-
+    search_fields = ('name',)
